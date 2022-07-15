1e356034013e156f24dd6ddc051d83be93bedf80
fbrcode <fabio.bressler@gmail.com>
2022-07-15 06:42:22 -0300

    header rename to manual

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Header from '../components/Header';
+import ManualHeader from '../components/ManualHeader';
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* header + connect button + nav bar */}
-      <Header />
+      <ManualHeader />
       Hello!
     </div>
   );
