350e98f18a91b20780fce97868a08aaf422fa95d
fbrcode <fabio.bressler@gmail.com>
2022-07-19 16:28:27 -0300

    change layout

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -26,7 +26,11 @@ export default function LotteryEntrance() {
 
   const dispatch = useNotification();
 
-  const { runContractFunction: enterLottery } = useWeb3Contract({
+  const {
+    runContractFunction: enterLottery,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
     abi: lotteryABI,
     contractAddress: lotteryAddress,
     functionName: 'enterLottery',
@@ -91,23 +95,34 @@ export default function LotteryEntrance() {
   // NEXT STEP: handle events listening to refresh the UI
 
   return (
-    <div>
-      Lottery entrance component
+    <div className="p-5">
       {lotteryAddress ? (
         <div>
           <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={async function () {
               await enterLottery({
                 onSuccess: handleSuccess, // checks to see if a transaction was sent successfully to MetaMask
                 onError: (error) => console.log(error),
               });
             }}
+            disabled={isLoading || isFetching}
           >
-            Enter Lottery
+            {isLoading || isFetching ? (
+              <div className=" animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+            ) : (
+              <div>Enter Lottery</div>
+            )}
           </button>
-          Lottery Entrance Fee = {ethers.utils.formatUnits(lotteryEntranceFee, 'ether')} ETH Number of players =
-          {numberOfPlayers}
-          Recent winner = {recentWinner}
+          <div className="p-1">
+            Lottery Entrance Fee: <strong>{ethers.utils.formatUnits(lotteryEntranceFee, 'ether')} ETH</strong>
+          </div>
+          <div className="p-1">
+            Number of players: <strong>{numberOfPlayers}</strong>
+          </div>
+          <div className="p-1">
+            Recent winner: <strong>{recentWinner}</strong>{' '}
+          </div>
         </div>
       ) : (
         <div>
